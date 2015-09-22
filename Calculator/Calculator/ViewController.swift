e4a70389dc54cc457fdffad6b7a781797616134f
Montchat <je4440@gmail.com>
2015-09-22 17:02:14 -0400

    added some challenges
    with the exception of the decimal button challenge

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,8 +12,21 @@ var number1 = ""
 var number2 = ""
 var operation: OperationType?
 
+var num1 = Double(number1)
+var num2 = Double(number2)
+
+func numberIsDivisible(number: Double) -> Bool {
+    if number % 1 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBOutlet weak var operationDisplay: UILabel!
     
     @IBAction func numberPressed(button: UIButton) {
         let num = button.titleLabel!.text!
@@ -34,30 +47,55 @@ class ViewController: UIViewController {
         number2 = ""
         operation = nil
         displayLabel.text = "0"
+        operationDisplay.text = ""
         
     }
     
     @IBAction func changeSign() {
-//        number1 = "\(-number1!)"
-        
+        if operation == nil {
+            num1 = num1! * -1
+            if numberIsDivisible(num1!) == true {
+                displayLabel.text = "\(Int(num1!))"
+                
+            } else {
+                displayLabel.text = "\(num1!))"
+                
+            }
+
+        } else {
+            num2 = num2! * -1
+            if numberIsDivisible(num2!) == true {
+                displayLabel.text = "\(Int(num2!))"
+                
+            } else {
+                displayLabel.text = "\(num2!))"
+                
+            }
+            
+        }
         
     }
     
     @IBAction func setOperation(button:UIButton) {
         let opSign = button.titleLabel!.text!
         operation = OperationType(rawValue: opSign) //this is a function that creates a new operation type with a chosen value based on the string that we are passing in
+        operationDisplay.text = opSign
     
     }
     
     @IBAction func runEquation() {
-        
-        let num1 = Double(number1)
-        let num2 = Double(number2)
+
         let total = calculateNumbers(n1: num1!, n2: num2!, operation: operation!)
         
-        displayLabel.text = "\(total)"
-        number1 = displayLabel.text!
+        if numberIsDivisible(total) {
+            displayLabel.text = "\(Int(total))"
+        } else {
+            displayLabel.text = "\(total)"
+            
+        }
         
+        number1 = displayLabel.text!
+        operationDisplay.text = ""
         
     }
     
